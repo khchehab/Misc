453607368a801d1e34b6be7a39f102a241e2f2b6
khchehab <khchehab@gmail.com>
2021-01-06 23:45:42 +0200

    Adding debug targets

--- Makefile
+++ Makefile
@@ -9,16 +9,18 @@ BUILD=out
 
 # Files
 EXECUTABLE=main
-SOURCE=$(SRC)/main.c $(SRC)/hashtable.c
+SOURCE=$(SRC)/main.c #$(SRC)/hashtable.c
 
 # Flags
-CC_FLAGS=-std=c11 -fsanitize=address
-
+CC_FLAGS=-std=c11
 
 all: clean main
-	@./$(BUILD)/$(EXECUTABLE)
+	@$(EXEC_SANITIZE_FLAGS) ./$(BUILD)/$(EXECUTABLE)
 
-debug: CC_FLAGS+=-DDEBUG
+# todo brew install clang to use its sanitizer instead of apple's clang
+
+debug: CC_FLAGS+=-DDEBUG -g -O0 -fsanitize=address -fno-omit-frame-pointer
+debug: EXEC_SANITIZE_FLAGS=ASAN_OPTIONS=symbolize=1:detect_leaks=1 ASAN_SYMBOLIZER_PATH=/usr/local/Cellar/llvm/11.0.0_1/bin/llvm-symbolizer
 debug: all
 
 main: $(BUILD)
--- src/main.c
+++ src/main.c
@@ -1,24 +1,29 @@
-#include "hashtable.h"
+// #include "hashtable.h"
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
 
-    hash_table* hash_table = hash_table_init(6);
+    // hash_table* hash_table = hash_table_init(6);
 
-    hash_table_insert(hash_table, 120, 34);
-    hash_table_insert(hash_table, 250, 44);
-    hash_table_insert(hash_table, 33, 52);
-    hash_table_insert(hash_table, 42, 56);
-    hash_table_insert(hash_table, 251, 178);
-    hash_table_insert(hash_table, 522, 23);
-    hash_table_print(hash_table);
+    // hash_table_insert(hash_table, 120, 34);
+    // hash_table_insert(hash_table, 250, 44);
+    // hash_table_insert(hash_table, 33, 52);
+    // hash_table_insert(hash_table, 42, 56);
+    // hash_table_insert(hash_table, 251, 178);
+    // hash_table_insert(hash_table, 522, 23);
+    // hash_table_print(hash_table);
 
-    hash_table_remove(hash_table, 120);
-    hash_table_remove(hash_table, 522);
-    hash_table_remove(hash_table, 521);
-    hash_table_remove(hash_table, 251);
-    hash_table_print(hash_table);
+    // hash_table_remove(hash_table, 120);
+    // hash_table_remove(hash_table, 522);
+    // hash_table_remove(hash_table, 521);
+    // hash_table_remove(hash_table, 251);
+    // hash_table_print(hash_table);
 
-    hash_table_free(hash_table);
+    // hash_table_free(hash_table);
+
+    int size = 10;
+    int* a = malloc(size * sizeof(int));
+    // free(a);
 
     return 0;
 }
