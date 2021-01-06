80a80e34a542f4f75eac9e6e98e585aad7d358d8
khchehab <khchehab@gmail.com>
2021-01-06 11:17:03 +0200

    Started implementing hash map functions

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "hashmap.h"
+
+// typedef struct hash_entry {
+//     int key;
+//     int value;
+//     hash_entry* next;
+// } hash_entry;
+
+// typedef struct hash_map {
+//     int size;
+//     hash_entry** entries;
+// } hash_map;
+
+#define SIZE 10
+
+unsigned int hash(hash_map* hash_map, int key);
+
+hash_map* hash_map_init() {
+    hash_map* hash_map = malloc(sizeof(hash_map));
+    hash_map->size = SIZE;
+    hash_map->entries = malloc(hash_map->size * sizeof(hash_entry*));
+    for(int i = 0; i < hash_map->size; i++) {
+        *(hash_map->entries + i) = NULL;
+    }
+    
+    return hash_map;
+}
+
+void hash_map_free(hash_map* hash_map) {
+    for(int i = 0; i < hash_map->size; i++) {
+        if(*(hash_map->entries + i) != NULL) {
+            free(*(hash_map->entries + i));
+        }
+    }
+    free(hash_map->entries);
+    hash_map->entries = NULL;
+    hash_map->size = 0;
+    free(hash_map);
+    hash_map = NULL;
+}
+
+bool hash_map_put(hash_map* hash_map, int key, int value) {
+
+}
+
+bool hash_map_get(hash_map* hash_map, int key) {
+
+}
+
+bool hash_map_remove(hash_map* hash_map, int key) {
+
+}
+
+void hash_map_print(hash_map* hash_map) {
+
+}
+
+// Hash function
+unsigned int hash(hash_map* hash_map, int key) {
+    return 0;
+}
--- src/hashmap.h
+++ src/hashmap.h
@@ -2,7 +2,22 @@
 #define __HASHMAP_H__
 
 typedef struct hash_entry {
-
+    int key;
+    int value;
+    hash_entry* next;
 } hash_entry;
+
+typedef struct hash_map {
+    int size;
+    hash_entry** entries;
+} hash_map;
+
+hash_map* hash_map_init();
+void hash_map_free(hash_map* hash_map);
+
+_Bool hash_map_put(hash_map* hash_map, int key, int value);
+_Bool hash_map_get(hash_map* hash_map, int key);
+_Bool hash_map_remove(hash_map* hash_map, int key);
+void hash_map_print(hash_map* hash_map);
 
 #endif // __HASHMAP_H__
