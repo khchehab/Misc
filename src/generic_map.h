8a7f15088e7d810d48685980fb381c9a9b8811bf
khchehab <khchehab@gmail.com>
2021-01-27 17:01:13 +0200

    WIP, trying to do a working map to then generalize it

---
+++ src/generic_map.h
@@ -0,0 +1,6 @@
+#ifndef __GENERIC_MAP_H__
+#define __GENERIC_MAP_H__
+
+
+
+#endif // __GENERIC_MAP_H__
--- src/main.c
+++ src/main.c
@@ -1,3 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// ---------------------------------------------------- //
+
+#define INITIAL_SIZE 10
+#define MAP_ENTRY_INCREMENTS 10
+
+typedef struct entry_t {
+    int key;
+    int value;
+} entry_t;
+
+typedef struct map_t {
+    int size;
+    int count;
+    entry_t* entries;
+} map_t;
+
+/*
+ * Initialize the map.
+ */
+map_t map_init() {
+    map_t map;
+    map_clear(&map);
+    return map;
+}
+
+/*
+ * Free the map entries from memory.
+ */
+void map_free(map_t* map) {
+    free(map->entries);
+}
+
+/*
+ * Reset the map to its initial state.
+ */
+void map_clear(map_t* map) {
+    map->size = INITIAL_SIZE;
+    map->count = 0;
+
+    // free the current entries memory
+    if(map->entries != NULL) {
+        free(map->entries);
+    }
+
+    // allocate memory for the entries
+    map->entries = malloc(map->size * sizeof(entry_t));
+}
+
+/*
+ * Checks if a key exists in the map.
+ */
+bool map_key_exist(map_t* map, int key) {
+
+}
+
+// value exist - optional
+// get
+// put
+// remove by key
+// print
+// helper functions for growing and shrinking
+
+// ---------------------------------------------------- //
+
 int main(int argc, char* argv[]) {
     return 0;
 }
